aec37c154ae183c17059874d1321b202c05c70d4
Benoît Hubert <benoithubert@gmail.com>
2017-01-04 22:28:29 +0100

    fix bloody typo

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.1/build/pure-min.css" integrity="sha384-CCTZv2q9I9m3UOxRLaJneXrrqKwUNOzZ6NGEUMwHtShDJ+nCoiXJCAgi05KfkLGY" crossorigin="anonymous">
         <script src="https://use.fontawesome.com/b27c5eac40.js"></script>
         <?php wp_head(); ?>
-        <?php if(file_exists(__DIR__ . '/piwik_code.php') { include 'piwik_code.php;' } ?>
+        <?php if(file_exists(__DIR__ . '/piwik_code.php')) { include 'piwik_code.php;' } ?>
     </head>
 
     <body>
